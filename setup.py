06986cff3d40934c33261657c1863e84aca4b1b0
Steve Traylen <steve.traylen@cern.ch>
2019-01-23 14:37:10 +0100

    Prepare next

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys, os
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
-version = '1.4.4'
+version = '1.4.5'
 
 install_requires = [
     'pyyaml',
