0d3df848e4733bdccdf484dfe6550ed12c70cbd1
Steve Traylen <steve.traylen@cern.ch>
2019-02-05 11:40:17 +0100

    Push

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys, os
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
-version = '1.4.13'
+version = '1.4.21'
 
 install_requires = [
     'pyyaml',
