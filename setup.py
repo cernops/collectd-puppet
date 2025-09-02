39946e03f15fb90e903e720e88458096ee9091c9
Steve Traylen <steve.traylen@cern.ch>
2025-09-02 17:02:40 +0200

    Release 2.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys, os
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
-version = '2.1.0'
+version = '2.1.1'
 
 install_requires = [
     'pyyaml',
